595c0cbd5ecad16cb1859de839acd88d16aed616
resonifiapp <jpcromien@yahoo.ca>
2025-11-07 12:42:11 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import PlausiblePing from "./PlausiblePing.jsx";
+import PlausibleListener from "./PlausibleListener.jsx";
 
 const root = document.getElementById("root");
 createRoot(root).render(
   <>
     <PlausiblePing />
+    <PlausibleListener />
     <App />
   </>
 );
-
