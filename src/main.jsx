5180d7e55dc589c59bb6d3e5a2cf309beddfd822
resonifiapp <jpcromien@yahoo.ca>
2025-11-07 11:06:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import PlausiblePing from "./PlausiblePing.jsx";
 
 const root = document.getElementById("root");
-createRoot(root).render(<App />);
+createRoot(root).render(
+  <>
+    <PlausiblePing />
+    <App />
+  </>
+);
+
