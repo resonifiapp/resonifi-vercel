4deb9c5a993fbcd7fb68e5f8e1fd52ad8fd39e5b
resonifiapp <jpcromien@yahoo.ca>
2025-11-09 13:42:31 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-export default function Home() {
-  return (
-    <div style={{minHeight:'100vh',display:'grid',placeItems:'center',background:'#0b1220',color:'#7ff5e2'}}>
-      <h1 style={{fontSize:48,margin:0}}>HOME DEBUG</h1>
-    </div>
-  );
+import React from "react";
+import Home from "./pages/Home.jsx";
+
+export default function App() {
+  return <Home />;
 }
