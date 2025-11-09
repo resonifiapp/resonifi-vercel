529db409b7be78fdc0cb2c3f7dd91345a47e0d16
resonifiapp <jpcromien@yahoo.ca>
2025-11-09 11:42:14 -0500

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,9 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+// src/lib/utils.js
+// Minimal utility: join class names with no external deps.
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-} 
+export function cn(...args) {
+  // Flattens arrays, filters falsy, joins with spaces
+  return args.flat(Infinity).filter(Boolean).join(" ");
+}
+
+export default { cn };
